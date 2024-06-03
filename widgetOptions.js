15a9cd1f44aa29b23ad9ac8f20b776012b2f5f63
Julian Bowman <julianfbowman@gmail.com>
2024-06-03 14:04:54 -0400

    re-organized code and fixed time issue

--- widgetOptions.js
+++ widgetOptions.js
@@ -2,66 +2,16 @@ $(document).ready(function() {
     flatpickr(".map-date", {
         enableTime: true,
         dateFormat: "Y-m-d H:i",
-        defaultDate: new Date(new Date().setDate(new Date().getDate() - 7))
+        defaultDate: new Date()
     });
 
     attachEventHandlers();
+    initIFrame();
 });
 
 function attachEventHandlers() {
     $(".apply-button").click(function() {
-        // Get input values
-        let mapDate = $(".map-date").val();
-        const mapTitle = $(".map-title").val();
-        const tempUnits = $(".temp-units").val();
-        const windUnits = $(".wind-units").val();
-        const presUnits = $(".pres-units").val();
-        const state = $(".state").val();
-        const mapWidth = $(".map-width").val();
-        const mapHeight = $(".map-height").val();
-        const mapZoom = $(".map-zoom").val();
-        const mapCenter = $(".map-center").text().split(", ");
-        const mapCenterLat = mapCenter[0];
-        const mapCenterLng = mapCenter[1];
-
-        mapDate = mapDate.replace(" ", "+");
-        mapDate += ":00";
-        // Calculate time_end as 5 minutes after time_start
-        let timeStart = new Date(mapDate.replace("+", "T"));
-            timeStart.setMinutes(timeStart.getMinutes() + 5);
-
-            // Format time_end correctly
-            let timeEnd = new Date(timeStart);
-            let year = timeEnd.getFullYear();
-            let month = String(timeEnd.getMonth() + 1).padStart(2, '0');
-            let day = String(timeEnd.getDate()).padStart(2, '0');
-            let hours = String(timeEnd.getHours()).padStart(2, '0');
-            let minutes = String(timeEnd.getMinutes()).padStart(2, '0');
-            let timeEndString = `${year}-${month}-${day}+${hours}:${minutes}:00`;
-
-        // Update iframe attributes and src
-        let iframe = $("iframe");
-        let src = iframe.attr("src");
-
-        // Parse the existing URL parameters and update them with new values
-        let newSrc = updateQueryStringParameter(src, "name", mapTitle);
-        newSrc = updateQueryStringParameter(newSrc, "time_start", mapDate);
-        newSrc = updateQueryStringParameter(newSrc, "time_end", timeEndString);
-        newSrc = updateQueryStringParameter(newSrc, "units_temp", tempUnits);
-        newSrc = updateQueryStringParameter(newSrc, "units_wind", windUnits);
-        newSrc = updateQueryStringParameter(newSrc, "units_pres", presUnits);
-        newSrc = updateQueryStringParameter(newSrc, "state", state);
-        newSrc = updateQueryStringParameter(newSrc, "z", mapZoom);
-        newSrc = updateQueryStringParameter(newSrc, "lat", mapCenterLat);
-        newSrc = updateQueryStringParameter(newSrc, "lng", mapCenterLng);
-
-        iframe.attr("src", newSrc);
-        iframe.attr("width", mapWidth);
-        iframe.attr("height", mapHeight);
-
-        // Update the code in the textarea
-        const iframeCode = '<iframe src="' + newSrc + '" width="' + mapWidth + '" height="' + mapHeight + '" frameborder="0" scrolling="no" allowtransparency="no"></iframe>';
-        $("#widget-code textarea").val(iframeCode);
+        initIFrame();
     });
 
     window.addEventListener("message", function(event) {
@@ -88,4 +38,66 @@ function updateQueryStringParameter(uri, key, value) {
     else {
         return uri + separator + key + "=" + value;
     }
+}
+
+function timeFormatter(timeStart) {
+    // Format time_end correctly
+    let timeEnd = new Date(timeStart);
+    let year = timeEnd.getFullYear();
+    let month = String(timeEnd.getMonth() + 1).padStart(2, '0');
+    let day = String(timeEnd.getDate()).padStart(2, '0');
+    let hours = String(timeEnd.getHours()).padStart(2, '0');
+    let minutes = String(timeEnd.getMinutes()).padStart(2, '0');
+    let timeEndString = `${year}-${month}-${day}+${hours}:${minutes}:00`;
+
+    return timeEndString;
+}
+
+function initIFrame() {
+       // Get input values
+       let mapDate = $(".map-date").val();
+       const mapTitle = $(".map-title").val();
+       const tempUnits = $(".temp-units").val();
+       const windUnits = $(".wind-units").val();
+       const presUnits = $(".pres-units").val();
+       const state = $(".state").val();
+       const mapWidth = $(".map-width").val();
+       const mapHeight = $(".map-height").val();
+       const mapZoom = $(".map-zoom").text();
+       const mapCenter = $(".map-center").text().split(", ");
+       const mapCenterLat = mapCenter[0];
+       const mapCenterLng = mapCenter[1];
+
+       mapDate = mapDate.replace(" ", "+");
+       mapDate += ":00";
+
+       // Calculate time_end as 5 minutes after time_start
+       let timeStart = new Date(mapDate.replace("+", "T"));
+       timeStart.setMinutes(timeStart.getMinutes() + 5);
+
+       let timeEndString = timeFormatter(timeStart);
+
+       // Update iframe attributes and src
+       let iframe = $("iframe");
+       let src = iframe.attr("src");
+
+       // Parse the existing URL parameters and update them with new values
+       let newSrc = updateQueryStringParameter(src, "name", mapTitle);
+       newSrc = updateQueryStringParameter(newSrc, "time_start", mapDate);
+       newSrc = updateQueryStringParameter(newSrc, "time_end", timeEndString);
+       newSrc = updateQueryStringParameter(newSrc, "units_temp", tempUnits);
+       newSrc = updateQueryStringParameter(newSrc, "units_wind", windUnits);
+       newSrc = updateQueryStringParameter(newSrc, "units_pres", presUnits);
+       newSrc = updateQueryStringParameter(newSrc, "state", state);
+       newSrc = updateQueryStringParameter(newSrc, "z", mapZoom);
+       newSrc = updateQueryStringParameter(newSrc, "lat", mapCenterLat);
+       newSrc = updateQueryStringParameter(newSrc, "lng", mapCenterLng);
+
+       iframe.attr("src", newSrc);
+       iframe.attr("width", mapWidth);
+       iframe.attr("height", mapHeight);
+
+       // Update the code in the textarea
+       const iframeCode = '<iframe src="' + newSrc + '" width="' + mapWidth + '" height="' + mapHeight + '" frameborder="0" scrolling="no" allowtransparency="no"></iframe>';
+       $("#widget-code textarea").val(iframeCode);
 }
